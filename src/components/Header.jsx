b0f90a4d957e2a6680e9373556e6e23947111f18
blossom-babs <saphashb@gmail.com>
2020-09-19 13:49:24 +0100

    header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,29 @@ import React from "react";
 import HeaderImage from "../assets/header_img.png";
 // import Nav component
 import Nav from "./Nav";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 function Header() {
+  const backgroundStyle = {
+    backgroundImage: `url(${HeaderImage})`,
+  };
 
-    const backgroundStyle = {
-        backgroundImage: `url(${HeaderImage})`
-    }
-
+  // className="header"
   return (
-    <header style={backgroundStyle} className="header">
+    <header >
+      <div style={backgroundStyle} className="header"></div>
       <Nav />
+      <div className="grid">
+        <div className="icons">
+          <FacebookIcon className="icon" />
+          <InstagramIcon className="icon" />
+          <TwitterIcon className="icon" />
+          <LinkedInIcon className="icon" />
+        </div>
+      </div>
     </header>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Nav(){
+
+    function handleClick(){
+
+    }
+
     return(
         <nav className="nav flex space-between">
             <h1 className="nav__logo">HR<span className="nav__span">WORK</span></h1>
+            <div onClick={handleClick} className="nav__icon"><MenuIcon/></div>
             <ul className="nav__links flex space-around">
-                <li className="nav__link">Who we are</li>
-               <li className="nav__link">Packages</li>
-               <li className="nav__link">Blog</li>
-               <li className="nav__link">Careers</li>
-                <li className="nav__link">Contact</li>
+                <li>Who we are</li>
+               <li>Packages</li>
+               <li>Blog</li>
+               <li>Careers</li>
+                <li>Contact</li>
             </ul>
+           
         </nav>
     )
 }
