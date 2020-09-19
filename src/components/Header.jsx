0e7debb8bf5abc5c0e5550a52c6765bea6e3848e
blossom-babs <saphashb@gmail.com>
2020-09-19 03:21:57 +0100

    work on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import HeaderImage from '../assets/header_img.png'
+// eslint-disable-next-line
 
-function Header(){
-     const bgStyle = {
-        background: `url(${HeaderImage})`,
-        backgroundSize: 'fill',
-        backgroundRepeat: 'no-repeat',
-        borderBottomRadius: '200px',
-//         backgroundColor: 'rgba(0, 0, 0, 0.86)',
-// clipPath: 'polygon(50% 0%, 100% 0, 100% 35%, 100% 65%, 83% 78%, 55% 84%, 28% 79%, 0% 70%, 0% 35%, 0 0)'
-clipPath: `polygon(50% 0%, 
-    100% 0, 100% 55%, 100% 65%, 60% 84%, 60% 84%, 
-    30% 79%, 0% 70%, 0% 35%, 0 0)`
+import React from "react";
+// import images
+import HeaderImage from "../assets/header_img.png";
+// import Nav component
+import Nav from "./Nav";
 
+function Header() {
+
+    const backgroundStyle = {
+        backgroundImage: `url(${HeaderImage})`
     }
-    return(
-        <header src={`${HeaderImage}`} className="header">
-            <img className="header__image" style={bgStyle}  alt=""/>
-        </header>
-    )
+
+  return (
+    <header style={backgroundStyle} className="header">
+      <Nav />
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+
 function Nav(){
     return(
-        <div>This is the nav</div>
+        <nav className="nav flex space-between">
+            <h1 className="nav__logo">HR<span className="nav__span">WORK</span></h1>
+            <ul className="nav__links flex space-around">
+                <li className="nav__link">Who we are</li>
+               <li className="nav__link">Packages</li>
+               <li className="nav__link">Blog</li>
+               <li className="nav__link">Careers</li>
+                <li className="nav__link">Contact</li>
+            </ul>
+        </nav>
     )
 }
 
